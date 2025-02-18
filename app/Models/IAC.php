5670adba81d6d95377495b78e690b57b6f33aaf5
DarrenJay1 <darrenjay134@gmail.com>
2025-02-18 11:28:08 +0800

    DARREN FEB 18

--- app/Models/IAC.php
+++ app/Models/IAC.php
@@ -18,6 +18,7 @@ class IAC extends Model
      * @var array<int, string>
      */
     protected $fillable = [
+            'disc_ID',
             'Country_of_Citizenship' ,
             'Last_Name',
             'First_Name',
@@ -32,7 +33,7 @@ class IAC extends Model
             'College_Unit',
             'Deparment/Institutions',
             'Contact_Number',
-            'Nature_of_Contribution',
+            'Nature_of_Contribution_Role',
             'Percentage_of_Contribution',
             'Date_of_Birth',
             'Civil_Status',
